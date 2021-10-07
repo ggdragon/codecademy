371d195ea6daa37d988560b499601e802c6d77ba
ggdragon <dtang10@gmail.com>
2021-10-07 15:53:59 -0700

    Update dogFactory.js

--- exercises/dogFactory.js
+++ exercises/dogFactory.js
@@ -1,3 +1,12 @@
+/* Write a function, dogFactory(). It should:
+  - have 3 parameters: name, breed, and weight (in that order) 
+  - expect name and breed to be strings 
+  - expect weight to be a number 
+  - return an object 
+  - have each of those parameters as keys on the returned object 
+  - returned with the values of the arguments that were passed in */
+
+
 function dogFactory(name, breed, weight) {
   return {
     _name: name,
